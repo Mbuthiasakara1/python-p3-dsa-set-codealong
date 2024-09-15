3a643c6161c03e13a5d2252022aea15c4de2f5e4
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-09-15 22:27:51 +0300

    data structures

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self,list =[]):
+        self.dictionary={}
+        for i in list:
+            self.dictionary[i] =True
 
-    pass
+    def has(self,value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value] =True
+        return self
+    
+    def delete(self, value):
+      self.dictionary.pop(value, None)
+      return self
+    
+    def size(self):
+      return len(self.dictionary)
